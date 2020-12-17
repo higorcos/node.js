90eecbb5f64a78fd0068e9a552c5601df78cbe53
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-12-17 13:02:30 -0300

    Hello Word.js

---
+++ 1. Hello Word/app.js
@@ -0,0 +1,11 @@
+var mostrarResultado = true;
+const site = 'https://github.com/higorcos';
+
+console.log('Hello Word');
+console.log('Meu nome é higor');
+console.log('Conheça o meu trabanho no Github');
+
+
+if(mostrarResultado){ //só vai mostra o site de for verdadeiro 
+  console.log(site);
+}
