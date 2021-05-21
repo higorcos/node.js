8eb24fd59aeb24a32b092940dd390c0cc4ce8677
higor costa <higorcosta2001@outlook.com>
2021-05-20 23:01:19 -0300

    dado

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-//static não precisa criar uma objeto para usar a classe
-class Calculadora {
-  static somar(a,b){
-    console.log(a+b)
+//dados 
+
+class Dado{
+  constructor(lado){
+    this.lado = lado
   }
-  static subtração(a,b){
-    console.log(a-b)
+  lançar = () => {
+    
+    for(var i=0; i !=3; i++){
+    var ladoQueCaiu = Math.floor((Math.random()* this.lado)+1)
+    console.log(ladoQueCaiu)
+    }
   }
 }
+dado1 = new Dado(3) // cria uma instância com o número de lados desejado 
+dado1.lançar()
 
-Calculadora.somar(20,80);
-Calculadora.subtração(90,76);
+
+//ou 
+//dados 
+/*
+class Dado{
+  constructor(){
+    this.lado = 0
+  }
+  static lançar = (max) => {
+    
+    for(var i=0; i !=1000; i++){
+    var ladoQueCaiu = Math.floor((Math.random()* max)+1)
+    console.log(ladoQueCaiu)
+    }
+  }
+}
+Dado.lançar(100)
+*/
